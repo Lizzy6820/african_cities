fcba0f60ec33499c1cdbc5e1b88f1f9fab50c6c5
John Doe <elizabeth.lwetabe@gmail.com>
2022-05-06 12:29:51 +0300

    nodemon

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import {} from 'dotenv/config.js'
 
 import postRoutes from './routes/posts.js';
 
@@ -14,10 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://user1:liz123456@cluster0.3dqru.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
